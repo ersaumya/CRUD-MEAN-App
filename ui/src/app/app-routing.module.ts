47f75715bc8ba3b7f247178930b1f314dc19174a
ersaumya <ersaumyarout@gmail.com>
2020-06-30 17:09:15 +0530

    list create component

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'',component:ListComponent},
+  {path:'create',component:CreateComponent},
+  {path:'edit/:id',component:CreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
