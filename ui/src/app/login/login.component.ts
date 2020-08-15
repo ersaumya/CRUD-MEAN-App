a52f02110e5e30dfc0da8ceb6284dc0fd11bed14
ersaumya <ersaumyarout@gmail.com>
2020-08-15 12:52:04 +0530

    login template added

--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor() {}
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
   }
-
+  loginUser(){
+    console.log(this.form.value);
+  }
 }
